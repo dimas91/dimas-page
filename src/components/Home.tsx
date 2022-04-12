60cf0dd1201a7ac7d5ba77122018c23a18c24b74
Dimas Tichelaar <dimas.tichelaar@gmail.com>
2022-04-12 11:22:14 +0200

    Start to woordeltje

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1 style={{ fontWeight: "normal", fontSize: "400%" }}>DIMAS TICHELAAR</h1>
+      <p>I am currently working on my personal website. Please come back later to get to know more about me. Until then, you can find me here:</p>
+      <ul style={{ display: "flex", justifyContent: "space-between", gap: "2em" }}>
+        <li><i className={"fab fa-linkedin fa-lg"}></i><a href="https://www.linkedin.com/in/dimas-tichelaar/" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Linkedin</a></li>
+        <li><i className={"fab fa-twitter fa-lg"}></i><a href="https://twitter.com/Dimas091" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Twitter</a></li>
+        <li><i className={"fab fa-github fa-lg"}></i><a href="https://github.com/dimas91" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Github</a></li>
+        <li><i className={"fab fa-soundcloud fa-lg"}></i><a href="https://soundcloud.com/dimas-music" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Soundcloud</a></li>
+      </ul>
+      <p>Or send me an email: dimas.tichelaar at gmail dot com</p>
+      <p>❤ - Dimas</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { 
+  BrowserRouter,
+  Routes,
+  Route } from "react-router-dom";
+import './index.css';
+import App from './App';
+import Woordeltje from './routes/Woordeltje';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="woordeltje" element={<Woordeltje />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
