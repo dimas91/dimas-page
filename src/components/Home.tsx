731aa66d829ffc02ea465803f648772c38c26178
Dimas Tichelaar <dimas.tichelaar@gmail.com>
2025-12-09 21:32:02 +0100

    🐛 Fix instagram logo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
       <ul style={{ display: "flex", justifyContent: "space-between", gap: "2em" }}>
         <li><i className={"fab fa-linkedin fa-lg"}></i><a href="https://www.linkedin.com/in/dimas-tichelaar/" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Linkedin</a></li>
         {/* <li><i className={"fab fa-twitter fa-lg"}></i><a href="https://twitter.com/Dimas091" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Twitter</a></li> */}
-        <li><i className={"fab fa-square-instagram fa-lg"}></i><a href="https://www.instagram.com/dimas91" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Instagram</a></li>
+        <li><i className={"fab fa-instagram-square fa-lg"}></i><a href="https://www.instagram.com/dimas91" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Instagram</a></li>
         <li><i className={"fab fa-github fa-lg"}></i><a href="https://github.com/dimas91" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Github</a></li>
         <li><i className={"fab fa-soundcloud fa-lg"}></i><a href="https://soundcloud.com/dimas-music" target="_blank" rel="noreferrer" style={{ padding: "0em .5em" }}>Soundcloud</a></li>
       </ul>
